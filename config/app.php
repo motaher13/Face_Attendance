35433d01d12194cfe9f9780013cc44b42bd0e7d2
ratul <ratulcse27@gmail.com>
2017-09-22 20:04:33 +0600

    add basic packages

--- config/app.php
+++ config/app.php
@@ -178,6 +178,8 @@ return [
         App\Providers\RouteServiceProvider::class,
         PrettyRoutes\ServiceProvider::class,
 
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
     ],
 
     /*
@@ -226,7 +228,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
     ],
 
 ];
