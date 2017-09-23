626dfbe23ce84c2f6d2b96467c7c6b6f963574d5
JOY <md.nayeem.iqubal@gmail.com>
2017-09-23 17:53:48 +0600

    Mix Set up Completed

--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -1,22 +1,6 @@
-<!-- BEGIN GLOBAL MANDATORY STYLES -->
-<link rel="stylesheet" href="{!! asset('assets/Ionicons/css/ionicons.min.css') !!}">
-
-
-<link rel="stylesheet" href="{!! asset('assets/global/fonts/google-fonts.css') !!}">
-<link rel="stylesheet" href="{!! asset('assets/global/plugins/font-awesome/css/font-awesome.min.css') !!}">
+{{--<!-- BEGIN GLOBAL MANDATORY STYLES -->--}}
 <link rel="stylesheet" href="{!! asset('assets/global/plugins/simple-line-icons/simple-line-icons.min.css') !!}">
-<link rel="stylesheet" href="{!! asset('assets/global/plugins/bootstrap/css/bootstrap.min.css') !!}">
-<link rel="stylesheet" href="{!! asset('assets/global/plugins/toastr/toastr.min.css') !!}">
-<!-- END GLOBAL MANDATORY STYLES -->
-<!-- BEGIN PAGE LEVEL PLUGINS -->
-<!-- END PAGE LEVEL PLUGINS -->
-<!-- BEGIN THEME GLOBAL STYLES -->
-<link rel="stylesheet" href="{!! asset('assets/global/css/components.min.css') !!}">
-<!-- END THEME GLOBAL STYLES -->
-<!-- BEGIN THEME LAYOUT STYLES -->
-<link rel="stylesheet" href="{!! asset('assets/layouts/layout/css/layout.min.css') !!}">
-<link rel="stylesheet" href="{!! asset('assets/layouts/layout/css/themes/darkblue.min.css') !!}">
-<link rel="stylesheet" href="{!! asset('assets/layouts/layout/css/custom.min.css') !!}">
+<link rel="stylesheet" href="{!! asset('css/app.css') !!}">
 <link rel="shortcut icon" href="favicon.ico" />
 @yield('styles')
 <!-- END THEME LAYOUT STYLES -->
