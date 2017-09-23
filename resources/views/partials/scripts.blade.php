b67bdece71f367b21436d357b5cf94fc61a4edb6
JOY <md.nayeem.iqubal@gmail.com>
2017-09-23 17:53:48 +0600

    Mix Set up Completed

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,19 +1,5 @@
-<!--[if lt IE 9]>
-<script type="text/javascript" src="{!! asset('assets/global/plugins/respond.min.js') !!}"></script>
-<script type="text/javascript" src="{!! asset('assets/global/plugins/excanvas.min.js') !!}"></script>
-<script type="text/javascript" src="{!! asset('assets/global/plugins/ie8.fix.min.js') !!}"></script>
-<![endif]-->
-<!-- BEGIN CORE PLUGINS -->
-<script type="text/javascript" src="{!! asset('assets/global/plugins/jquery.min.js') !!}"></script>
-<script type="text/javascript" src="{!! asset('assets/global/plugins/bootstrap/js/bootstrap.min.js') !!}"></script>
-<script type="text/javascript" src="{!! asset('assets/global/plugins/toastr/toastr.min.js') !!}"></script>
-<!-- END CORE PLUGINS -->
-@include('partials.toastr')
 
-<!-- BEGIN THEME GLOBAL SCRIPTS -->
-<script type="text/javascript" src="{!! asset('assets/global/scripts/app.min.js') !!}"></script>
-<!-- END THEME GLOBAL SCRIPTS -->
-<!-- BEGIN THEME LAYOUT SCRIPTS -->
-<script type="text/javascript" src="{!! asset('assets/layouts/layout/scripts/layout.min.js') !!}"></script>
-<!-- END THEME LAYOUT SCRIPTS -->
+<script type="text/javascript" src="{!! asset('js/app.js') !!}"></script>
+
+@include('partials.toastr')
 @yield('scripts')
