11e072e969e3fbb235ce2f7394ec8c6e3a582128
ratul <ratulcse27@gmail.com>
2017-09-23 15:00:56 +0600

    add web register

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ Route::get('/',['as'=>'home','uses'=>'Dashboard\MainDashboardController@home']);
 Route::group(['namespace' => 'Auth','middleware' => ['guest']],function (){
     Route::get('login',['as'=>'web.login','uses'=>'AuthController@login']);
     Route::post('login',['as'=>'web.do.login','uses'=>'AuthController@doLogin']);
-    /*Route::get('register',['as'=>'web.register','uses'=>'AuthController@register']);
-    Route::post('register',['as'=>'web.do.register','uses'=>'AuthController@doRegister']);*/
+    Route::get('register',['as'=>'web.register','uses'=>'AuthController@register']);
+    Route::post('register',['as'=>'web.do.register','uses'=>'AuthController@doRegister']);
 });
 
 Route::group(['middleware' => ['auth']],function (){
