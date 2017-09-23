b8d3b371e75cc18896996e6ec6385563357a258f
JOY <md.nayeem.iqubal@gmail.com>
2017-09-23 16:12:18 +0600

    Resource Code Refined

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,15 +17,15 @@
     <meta content="" name="description"/>
     <meta content="" name="author"/>
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
-    <link href="../assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-    <link href="../assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-    <link href="../assets/global/plugins/toastr/toastr.min.css" rel="stylesheet" type="text/css"/>
+    <link href="{!! asset('assets/global/plugins/font-awesome/css/font-awesome.min.css') !!}" rel="stylesheet" type="text/css"/>
+    <link href="{!! asset('assets/global/plugins/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet" type="text/css"/>
+    <link href="{!! asset('assets/global/plugins/toastr/toastr.min.css') !!}" rel="stylesheet" type="text/css"/>
     <!-- END GLOBAL MANDATORY STYLES -->
     <!-- BEGIN THEME GLOBAL STYLES -->
-    <link href="../assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css"/>
+    <link href="{!! asset('assets/global/css/components.min.css') !!}" rel="stylesheet" id="style_components" type="text/css"/>
     <!-- END THEME GLOBAL STYLES -->
     <!-- BEGIN PAGE LEVEL STYLES -->
-    <link href="../assets/pages/css/login.min.css" rel="stylesheet" type="text/css"/>
+    <link href="{!! asset('assets/pages/css/login.min.css') !!}" rel="stylesheet" type="text/css"/>
 
     <style>
         #particle {
@@ -59,63 +59,59 @@
 <!-- BEGIN LOGIN -->
 <div class="content overlay">
     <!-- END LOGIN FORM -->
-    {!! Form::open(['route' => 'web.do.register','method' => 'post']) !!}
-    <h3 class="font-green">Sign Up</h3>
-    <p class="hint"> Enter your account details below: </p>
+    <form action="{!! route('web.do.register') !!}" method="POST">
+        {!! csrf_field() !!}
+        <h3 class="font-green">Sign Up</h3>
+        <p class="hint"> Enter your account details below: </p>
 
-    <div class="form-group">
-        <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
-        {!! Form::label('name','E-Mail',['class' => 'control-label visible-ie8 visible-ie9']) !!}
-        {!! Form::text('name',null,['class' => 'form-control form-control-solid placeholder-no-fix','autocomplete' => 'off','placeholder' => 'Username','required']) !!}
-    </div>
+        <div class="form-group">
+            <label for="name" class="control-label visible-ie8 visible-ie9">Name</label>
+            <input type="text" name="name" class="form-control form-control-solid placeholder-no-fix" value="{!! old('name') !!}" placeholder="Username" autocomplete="off"  required/>
+        </div>
 
-    <div class="form-group">
-        <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
-        {!! Form::label('email','E-Mail',['class' => 'control-label visible-ie8 visible-ie9']) !!}
-        {!! Form::email('email',null,['class' => 'form-control form-control-solid placeholder-no-fix','autocomplete' => 'off','placeholder' => 'Email-Address','required']) !!}
-    </div>
 
-    <div class="form-group">
-        {!! Form::label('password','Password',['class' => 'control-label visible-ie8 visible-ie9']) !!}
-        {!! Form::password('password',['class' => 'form-control form-control-solid placeholder-no-fix','autocomplete' => 'off', 'placeholder' => 'Password','required']) !!}
-    </div>
+        <div class="form-group">
+            <label for="email" class="control-label visible-ie8 visible-ie9">E-Mail</label>
+            <input type="text" name="email" class="form-control form-control-solid placeholder-no-fix" value="{!! old('email') !!}" placeholder="Email-Address" autocomplete="off"  required/>
+        </div>
 
-    <div class="form-group">
-        {!! Form::label('password_confirmation','Re-type Your Password',['class' => 'control-label visible-ie8 visible-ie9']) !!}
-        {!! Form::password('password_confirmation',['class' => 'form-control form-control-solid placeholder-no-fix','autocomplete' => 'off', 'placeholder' => 'Re-type Your Password','required']) !!}
-    </div>
-    {{--<div class="form-group margin-top-20 margin-bottom-20">
-        <label class="mt-checkbox mt-checkbox-outline">
-            <input type="checkbox" name="tnc"/> I agree to the
-            <a href="javascript:;">Terms of Service </a> &
-            <a href="javascript:;">Privacy Policy </a>
-            <span></span>
-        </label>
-        <div id="register_tnc_error"></div>
-    </div>--}}
-    <div class="form-actions">
-        <a href="{!! route('web.login') !!}" class="btn green btn-outline">Back</a>
-        {{--<button type="button" id="register-back-btn" class="btn green btn-outline">Back</button>--}}
-        {!! Form::submit('Register', ['class' => 'btn btn-success uppercase pull-right']) !!}
-        {{--<button type="submit" id="register-submit-btn" class="btn btn-success uppercase pull-right">Submit</button>--}}
-    </div>
-    {!! Form::close() !!}
+
+        <div class="form-group">
+            <label for="password" class="control-label visible-ie8 visible-ie9">Password</label>
+            <input type="password" name="password" class="form-control form-control-solid placeholder-no-fix" placeholder="Password" autocomplete="off"  required/>
+        </div>
+
+
+        <div class="form-group">
+            <label for="password_confirmation" class="control-label visible-ie8 visible-ie9">Re-type Your Password</label>
+            <input type="password" name="password_confirmation" class="form-control form-control-solid placeholder-no-fix" placeholder="Re-type Your Password" autocomplete="off"  required/>
+        </div>
+
+        {{--<div class="form-group margin-top-20 margin-bottom-20">
+            <label class="mt-checkbox mt-checkbox-outline">
+                <input type="checkbox" name="tnc"/> I agree to the
+                <a href="javascript:;">Terms of Service </a> &
+                <a href="javascript:;">Privacy Policy </a>
+                <span></span>
+            </label>
+            <div id="register_tnc_error"></div>
+        </div>--}}
+        <div class="form-actions">
+            <a href="{!! route('web.login') !!}" class="btn green btn-outline">Back</a>
+            {{--<button type="button" id="register-back-btn" class="btn green btn-outline">Back</button>--}}
+            <input type="submit" name="submit" class="btn btn-success uppercase pull-right" value="Register"/>
+            {{--<button type="submit" id="register-submit-btn" class="btn btn-success uppercase pull-right">Submit</button>--}}
+        </div>
+    </form>
 
 </div>
 <div class="copyright"> {!! date('Y') !!} © {!! \App\BaseSettings\Settings::$company_name !!} </div>
-<!--[if lt IE 9]>
-<script src="../assets/global/plugins/respond.min.js"></script>
-<script src="../assets/global/plugins/excanvas.min.js"></script>
-<script src="../assets/global/plugins/ie8.fix.min.js"></script>
-<![endif]-->
-<!-- BEGIN CORE PLUGINS -->
-<script src="../assets/global/plugins/jquery.min.js" type="text/javascript"></script>
-<script src="../assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="../assets/global/plugins/toastr/toastr.min.js" type="text/javascript"></script>
-<script src="../assets/particles/particles.min.js"></script>
+
+<script src="{!! asset('assets/global/plugins/jquery.min.js') !!}" type="text/javascript"></script>
+<script src="{!! asset('assets/particles/particles.min.js') !!}"></script>
 <script>
     particlesJS.load('particle', '../assets/particles.json', function() {
-        //console.log('callback - particles.js config loaded');
+        console.log('callback - particles.js config loaded');
     });
 </script>
 <!-- END CORE PLUGINS -->
