4be72c25311f39a7ac7ae6a1a6568313800b64a4
Masiur Rahman Siddiki <mrsiddiki@gmail.com>
2017-11-08 18:04:28 +0600

    password reset added and related bug fixed

---
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Masiur
+ * Date: 11/8/17
+ * Time: 5:43 PM
+ */
+
+namespace App\Http\Controllers\Auth;
+
+
+class ForgotPasswordController
+{
+
+}
---
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Masiur
+ * Date: 11/8/17
+ * Time: 5:45 PM
+ */
+
+namespace App\Http\Controllers\Auth;
+
+
+class ResetPasswordController
+{
+
+}
