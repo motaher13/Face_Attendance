5e350abcbc92749741848de8e50b36caab26f457
motaher hossain <motaher1333@gmail.com>
2018-07-11 10:56:00 +0600

    first commit

--- app/Repositories/userInfoRepository.php
+++ app/Repositories/userInfoRepository.php
@@ -1,37 +1,15 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Motaher
- * Date: 2/9/2018
- * Time: 8:12 PM
+ * User: motaher
+ * Date: 7/9/18
+ * Time: 10:21 PM
  */
 
 namespace App\Repositories;
 
-use App\Models\UserInfo;
 
-
-class userInfoRepository extends Repository
+class userInfoRepository
 {
 
-    /**
-     * Specify Model class name
-     *
-     * @return mixed
-     */
-    public function model()
-    {
-        return UserInfo::class;
-    }
-
-    /**
-     * Filter data based on user input
-     *
-     * @param array $filter
-     * @param       $query
-     */
-    public function filterData(array $filter, $query)
-    {
-        // TODO: Implement filterData() method.
-    }
 }
